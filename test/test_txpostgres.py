67d9c6b93ec953240a6e99c9b72127b0f69ae3b8
Jan Urbański <wulczer@wulczer.org>
2012-05-30 13:39:23 +0200

    Allow overriding the test database config with environment variables.

--- test/test_txpostgres.py
+++ test/test_txpostgres.py
@@ -4,6 +4,7 @@
 """
 Tests for twisted.enterprise.pgadbapi.
 """
+import os
 
 try:
     import psycopg2
@@ -23,10 +24,10 @@ from twisted.python import failure
 
 simple_table_schema = "CREATE TABLE simple (x integer)"
 
-DB_NAME = "twisted_test"
-DB_HOST = "localhost"
-DB_USER = "twisted_test"
-DB_PASS = "twisted_test"
+DB_NAME = os.getenv("TXPOSTGRES_TEST_DATABASE", "twisted_test")
+DB_HOST = os.getenv("TXPOSTGRES_TEST_HOST", "localhost")
+DB_USER = os.getenv("TXPOSTGRES_TEST_USER", "twisted_test")
+DB_PASS = os.getenv("TXPOSTGRES_TEST_PASSWORD", "twisted_test")
 
 
 def getSkipForPsycopg2():
