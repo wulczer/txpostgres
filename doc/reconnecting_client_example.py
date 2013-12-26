1031baef2e6488c27f04af551bbcbb4cf7b941cb
Sylvain Francis <sylvain.francis@eyepea.eu>
2013-12-26 11:09:57 +0100

    Put ReconnectingClient in txpostgres.py

--- doc/reconnecting_client_example.py
+++ doc/reconnecting_client_example.py
@@ -1,80 +1,7 @@
 import logging
-from twisted.internet import reactor, task, defer
+from twisted.internet import reactor, task
 from twisted.python.util import println
-from txpostgres.txpostgres import Connection
-
-# noinspection PyProtectedMember
-class ReconnectingConnection(object):
-    """
-    Wrapper around txpostgres.connection that will keep trying to reconnect forever
-    The goals here are to suppress all connection errors and running queries without
-    having to worry about the underlying connection state.
-    """
-
-    def __init__(self, logger=None, recon_delay=3, debug=False, **connect_kwargs):
-        # For debugging purposes
-        defer.setDebugging(debug)
-        self.logger = logger if logger else logging.getLogger(__name__)
-        if not logger:
-            logging.basicConfig()
-            self.logger.setLevel(logging.DEBUG)
-
-        self.conn = None
-        # We use a state-machine like
-        self.state = 'not_connected'
-        # Delay before attempting to reconnect
-        self.recon_delay = recon_delay
-        # See self.connect
-        self.connection_args = connect_kwargs
-
-        self.pending_requests = {}
-
-    def connect(self):
-        """
-        Call the wrapped connection's connect, but catch whatever comes back, inlcuding future connection errors
-        """
-        self.conn = Connection(reconnector=self.connection_failed)
-        d = self.conn.connect(**self.connection_args)
-        d.addCallback(self.connection_made)
-        d.addErrback(self.connection_failed)
-
-    def connection_made(self, conn):
-        self.logger.debug('Connection was made!')
-        self.state = 'connected'
-        #If we had pending requests, now is the time to run them
-        for requestD, query in self.pending_requests.items():
-            d = self.conn.runQuery(*query[0], **query[1])
-            d.addCallback(requestD.callback)
-            d.addErrback(requestD.errback)
-            del(self.pending_requests[requestD])
-
-    def connection_failed(self, exc):
-        self.logger.info('Connection Failed, resetting it! (%s)', exc)
-
-        # Don't close the connection before it had a chance to reconnect
-        if self.state != 'reconnecting':
-            self.state = 'disconnected'
-            try:
-                self.conn.close()
-                del self.conn
-            except:
-                # Just. Keep. Running.
-                self.logger.warning('Could not cleanly close the connection.')
-        reactor.callLater(self.recon_delay, self.reconnect)
-
-    def reconnect(self):
-        self.connect()
-        self.state = 'reconnecting'
-
-    def runQuery(self, *args, **kwargs):
-        #TODO: add a some sort of deffered magic here, for when we are disconnected and want to start querying later
-        if self.state == 'connected':
-            return self.conn.runQuery(*args, **kwargs)
-        else:
-            #If we are not connected, remember the query to call it later, when we have a connection
-            d = defer.Deferred()
-            self.pending_requests[d] = (args, kwargs)
-            return d
+from txpostgres.txpostgres import ReconnectingConnection
 
 
 def print_query(conn, *_):
@@ -82,15 +9,11 @@ def print_query(conn, *_):
 
     conn.runQuery('select tablename from pg_tables').addCallback(println)
 
-
-def myprint(self, *args):
-    print args
-
 if __name__ == '__main__':
-    connection = ReconnectingConnection(debug=True,
-                                        database='postgres',
-                                        user='',
-                                        password='',
+    logging.basicConfig()
+    connection = ReconnectingConnection(database='postgres',
+                                        user='diallictive',
+                                        password='diallictive',
                                         port=5432,
                                         host='localhost')
     connection.connect()
--- txpostgres/txpostgres.py
+++ txpostgres/txpostgres.py
@@ -40,6 +40,8 @@ documentation_
 .. _issue tracker: https://github.com/wulczer/txpostgres/issues
 .. _documentation: http://txpostgres.readthedocs.org/
 """
+import logging
+from twisted.internet import reactor
 
 try:
     import psycopg2
@@ -922,3 +924,70 @@ class ConnectionPool(object):
         c = self.connections.pop()
         d = c.runInteraction(interaction, *args, **kwargs)
         return d.addBoth(self._putBackAndPassthrough, c)
+
+
+class ReconnectingConnection(object):
+    """
+    Wrapper around txpostgres.connection that will keep trying to reconnect forever
+    The goals here are to suppress all connection errors and running queries without
+    having to worry about the underlying connection state.
+    """
+
+    def __init__(self, logger=None, recon_delay=3, debug=False, **connect_kwargs):
+        self.logger = logging.getLogger(__name__)
+        self.conn = None
+        # We use a state-machine like
+        self.state = 'not_connected'
+        # Delay before attempting to reconnect
+        self.recon_delay = recon_delay
+        # See self.connect
+        self.connection_args = connect_kwargs
+
+        self.pending_requests = {}
+
+    def connect(self):
+        """
+        Call the wrapped connection's connect, but catch whatever comes back, inlcuding future connection errors
+        """
+        self.conn = Connection(reconnector=self.connection_failed)
+        d = self.conn.connect(**self.connection_args)
+        d.addCallback(self.connection_made)
+        d.addErrback(self.connection_failed)
+
+    def connection_made(self, conn):
+        self.logger.debug('Connection was made!')
+        self.state = 'connected'
+        #If we had pending requests, now is the time to run them
+        for requestD, query in self.pending_requests.items():
+            d = self.conn.runQuery(*query[0], **query[1])
+            d.addCallback(requestD.callback)
+            d.addErrback(requestD.errback)
+            del(self.pending_requests[requestD])
+
+    def connection_failed(self, exc):
+        self.logger.info('Connection Failed, resetting it! (%s)', exc)
+
+        # Don't close the connection before it had a chance to reconnect
+        if self.state != 'reconnecting':
+            self.state = 'disconnected'
+            try:
+                self.conn.close()
+                del self.conn
+            except:
+                # Just. Keep. Running.
+                self.logger.warning('Could not cleanly close the connection.')
+        reactor.callLater(self.recon_delay, self.reconnect)
+
+    def reconnect(self):
+        self.connect()
+        self.state = 'reconnecting'
+
+    def runQuery(self, *args, **kwargs):
+        #TODO: add a some sort of deffered magic here, for when we are disconnected and want to start querying later
+        if self.state == 'connected':
+            return self.conn.runQuery(*args, **kwargs)
+        else:
+            #If we are not connected, remember the query to call it later, when we have a connection
+            d = defer.Deferred()
+            self.pending_requests[d] = (args, kwargs)
+            return d
